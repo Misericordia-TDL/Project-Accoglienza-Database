870a7ca46a6d4043cfeb117a0643cc3a353696ff
Orlando <mmoa33@hotmail.com>
2017-02-24 15:36:06 +0000

    Added test script to check whether the database is present in the mongdb Node

---
+++ test/checkif_db_exists.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Function to check if  a database exists
+# Syntax: checkif_db_exists db_name
+function checkif_db_exists {
+        local js="      var found=new Boolean(false);
+                        var dbs = db.getMongo().getDBNames(); 
+                        for(var i in dbs) {
+                                if (dbs[i].toString() == '"${1}"') 
+                                        found = 1;
+                        }
+                        if (found.valueOf()) print(true); else print(false);
+                        "
+        if [ $(mongo --eval "$js" --quiet) == "false" ]; then
+             echo "Database  $1  doesn't exist!"
+	else
+		echo "Database  $1  found!"
+        fi
+	
+}
+
+checkif_db_exists Accoglienza_Misericordia 
+
